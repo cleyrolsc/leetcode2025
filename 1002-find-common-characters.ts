01936e8abea71f3ec86c6a6d04d6e24da6316984
Cleyrol Santana <cleyrolsc@gmail.com>
2025-01-02 13:42:54 -0400

    Completed leetcode exercise 217 - Contain Duplicate

--- 1002-find-common-characters.ts
+++ 1002-find-common-characters.ts
@@ -2,7 +2,7 @@ function commonChars(words: string[]): string[] {
     let common = words[0].split('');
 
     for (let i = 1; i < words.length; i++) {
-        let newCommon = [];
+        let newCommon: string[] = [];
         let word = words[i].split('');
         for (let char of common) {
             let index = word.indexOf(char);
