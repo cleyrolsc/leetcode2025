57f5308fac08215f9ad63d50717ce97508eb6232
Cleyrol Santana <cleyrolsc@gmail.com>
2025-01-01 20:29:40 -0400

    Solved leetcode 1002, find common characters

---
+++ 1002-find-common-characters/1002.ts
@@ -0,0 +1,18 @@
+function commonChars(words: string[]): string[] {
+    let common = words[0].split('');
+
+    for (let i = 1; i < words.length; i++) {
+        let newCommon = [];
+        let word = words[i].split('');
+        for (let char of common) {
+            let index = word.indexOf(char);
+            if (index !== -1) {
+                newCommon.push(char);
+                word.splice(index, 1);
+            }
+        }
+        common = newCommon;
+    }
+
+    return common;
+};
