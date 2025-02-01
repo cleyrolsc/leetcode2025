a055509e5ec1dce60df17a400e79fb95f37b5d21
Cleyrol Santana <cleyrolsc@gmail.com>
2025-02-01 19:23:55 -0400

    Complete exercise - 2798 Number of Employees Who Met the Target

---
+++ Day33-2798-NumberofEmployeesWhoMettheTarget.ts
@@ -0,0 +1,9 @@
+function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
+    let c = 0;
+    for(let i = 0; i < hours.length; i++){
+        if(hours[i] >= target){
+            c++;
+        }
+    }
+    return c;
+};
